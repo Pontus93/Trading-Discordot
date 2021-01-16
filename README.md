d6f7e6daccd56e00f26b977f67dfb9a8e376c236
Pontus <pontuspersson@outlook.com>
2021-01-16 21:38:02 +0100

    v2.0 tradingBot

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 <strong>Borat the Bot!</strong> simple DiscordBot, pulls a few jokes with api and listen to simple commands. <br>
 
 
-[![](https://i.ibb.co/5WrDshS/Screenshot-2020-12-09-at-13-43-48.png)](#)
+[![](https://i.ibb.co/stsT2Mq/Screenshot-2021-01-16-at-21-32-13.png)](#)
--- bot.js
+++ bot.js
@@ -7,12 +7,8 @@ const fetch = require("node-fetch");
 // WEB Scraping.
 const puppeteer = require('puppeteer');
 
-
-
-
-
 // Random bot msg.
-const bullBear = ["Bear-cert!", "Bull-cert!"]
+const bullBear = ["tjäna", "förlora"]
 // Console.log when up and & running.
 client.on("ready", () => {
     console.log("The bot is up & running");
@@ -21,50 +17,76 @@ client.on("ready", () => {
 // Bot listen & answer.
 client.on('message', msg => {
     if (msg.content === "borat") {
-        msg.channel.send("Testa skriv bitcoin,cert eller joke")
+        msg.channel.send("Testa skriv bitcoin,trading eller joke.");
+        msg.channel.send("Du kan även skriva spectracure,prolight eller sinch.");
+        msg.channel.send("Vill du lägga till fler? skriv till raketpontan.");
     }
-    if (msg.content === "cert") {
+    if (msg.content === "trading") {
         let stockRandom = bullBear[Math.floor(Math.random() * bullBear.length)];
-        msg.channel.send("köp " + stockRandom + ", den sitter 100%")
+        msg.channel.send("Idag kommer du " + stockRandom + " pengar på börsen.")
     }
-    if (msg.content === "spectracure") {
-        scrapeProduct('https://spectracure.se/');
-        async function scrapeProduct(url) {
-            try {
-                const browser = await puppeteer.launch();
-                const page = await browser.newPage();
-                await page.goto(url);
+    // WebScraper for Stocks.
 
-                // Select by Xpath.
-                const [el] = await page.$x('/html/body/div[2]/div/div[2]/div/div/div/div/div/div[2]/div/div[2]/div/div/div/div/div[2]/div/div/div[6]/span[2]');
-                const txt = await el.getProperty('textContent');
-                rawTxt = await txt.jsonValue();
-                console.log(rawTxt);
-
-                browser.close();
-            }
-            catch (error) {
-                console.log(error);
-            }
-        }
-
-        msg.channel.send("Spectracurs kurs är: " + rawTxt + "kr");
-    }
 })
 
-// Fetch api & print message.
+// Fetch api/stockValue/bitcoinValue & prints message.
 client.on('message', async msg => {
     if (msg.content === "bitcoin") {
         const response = await fetch("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,SEK");
         const currency = await response.json();
         let btnUS = currency.USD;
-        msg.channel.send("Its booming man! " + btnUS + "$ right now!");
+        msg.channel.send("Its a wild ride, " + btnUS + "$ right now!");
     }
     if (msg.content === "joke") {
         const response = await fetch("https://api.chucknorris.io/jokes/random");
         const fact = await response.json();
         let chuck = fact.value;
         msg.channel.send(chuck)
+    }
+    if (msg.content === "spectracure") {
+        scrapeProduct('https://www.avanza.se/aktier/om-aktien.html/574898/spectracure');
+    }
+    if (msg.content === "sinch") {
+        scrapeProduct('https://www.avanza.se/aktier/om-aktien.html/599956/sinch');
+    }
+    if (msg.content === "prolight") {
+        scrapeProduct('https://www.avanza.se/aktier/om-aktien.html/741528/prolight-diagnostics')
+    }
+    if (msg.content === "aktie") {
+    }
+    async function scrapeProduct(url) {
+        try {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(url);
+
+            // Select by Xpath.
+            const [currentValueLive] = await page.$x('//*[@id="surface"]/div[3]/div/div/div/div/ul/li[6]/span[2]/span');
+            const [currentRevenueLive] = await page.$x('//*[@id="surface"]/div[5]/div[2]/div[2]/div/div[2]/div[1]/span');
+            const [yearlyProgressLive] = await page.$x('//*[@id="surface"]/div[5]/div[2]/div[6]/div/div/table/tbody/tr[4]/td[3]');
+            const [dailyProgressLive] = await page.$x('//*[@id="surface"]/div[3]/div/div/div/div/ul/li[2]/div[2]/span[2]');
+
+            const currentValue = await currentValueLive.getProperty('textContent');
+            const currentRevenue = await currentRevenueLive.getProperty('textContent');
+            const yearlyProgress = await yearlyProgressLive.getProperty('textContent');
+            const dailyProgress = await dailyProgressLive.getProperty('textContent');
+
+            StockValue = await currentValue.jsonValue();
+            revenueValue = await currentRevenue.jsonValue();
+            yearlyProgressValue = await yearlyProgress.jsonValue();
+            dailyProgressValue = await dailyProgress.jsonValue();
+
+            browser.close();
+        }
+        catch (error) {
+            console.log(error);
+            msg.channel.send("something wrong with the WebScraper please contact my creator");
+        }
+        msg.channel.send("Nuvarande kurs: " + StockValue + "kr");
+        msg.channel.send("Utveckling idag är: " + dailyProgressValue + "%");
+        msg.channel.send("Omsättning idag: " + revenueValue + "kr");
+        msg.channel.send("Utveckling i år: " + yearlyProgressValue + "%");
+        msg.channel.send("-----------------------------------------");
     }
 });
 
